fa53455ec7dc670e088243533bea292cf1af11cd
Brian Nickerson <nickerson.brian@gmail.com>
2016-07-20 20:35:45 -0400

    Implemented timeout callback. Not functioning as intended.

--- hello.js
+++ hello.js
@@ -1,8 +1,12 @@
 var http = require('http');
 
-http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.write('Hello, this is dog.');
-  res.end();
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  response.write('Dog is running.');
+  // This isn't working as intended. We should get 'Dog is running.' immediately, and then 'Dog is done running.' 5 seconds later.
+  // Instead, we're getting both messages at the same time, when the function ends. Not sure why that's happening.
+  setTimeout(function(){
+    response.end('Dog is done running.');
+  }, 5000);
 }).listen(8080);
 console.log('Listening to port 8080...');
